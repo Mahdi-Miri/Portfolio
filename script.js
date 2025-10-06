496b94578e566143137541de6f4d522366dc1f24
Mahdi Miri <mahdimiri7@gmail.com>
2025-10-06 16:24:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const teamMembers = document.querySelectorAll('.team-member');
 
+    teamMembers.forEach(member => {
+        member.addEventListener('click', () => {
+            const url = member.getAttribute('data-url');
+            if (url) {
+                // Open the link in a new tab
+                window.open(url, '_blank');
+            }
+        });
+    });
+});
